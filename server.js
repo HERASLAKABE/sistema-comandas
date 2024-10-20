3650753cd964f07aad18f14aaeaeefefe5a2170b
HERASLAKABE <ikerlakabe@gmail.com>
2024-10-20 20:17:40 +0200

    Corrección de interfaz de cocina y nuevas funcionalidades

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+const cors = require('cors');
+app.use(cors());
+
 // Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,9 +30,13 @@ app.post('/pedido', (req, res) => {
 
   nuevoPedido.id = Date.now(); // Asignar un ID único
   nuevoPedido.estado = 'Pendiente';
+  nuevoPedido.platos = nuevoPedido.platos.map(plato => ({
+    ...plato,
+    estado: 'Pendiente'
+  }));
   pedidos.push(nuevoPedido);
 
-  // Emitir evento de nuevo pedido a las pantallas de cocina y barra
+  // Emitir evento de nuevo pedido
   io.emit('nuevoPedido', nuevoPedido);
 
   res.status(200).send({ mensaje: 'Pedido recibido' });
@@ -42,20 +49,35 @@ io.on('connection', (socket) => {
   // Enviar lista de pedidos actuales al conectarse
   socket.emit('pedidosActuales', pedidos);
 
-  // Escuchar eventos de actualización de estado
-  socket.on('actualizarEstado', (data) => {
+  // Escuchar eventos de actualización de estado de platos
+  socket.on('actualizarEstadoPlato', (data) => {
+    const { idPedido, idPlato } = data;
+    const pedido = pedidos.find((p) => p.id === idPedido);
+    if (pedido) {
+      const plato = pedido.platos.find((pl) => parseInt(pl.id) === idPlato);
+      if (plato) {
+        plato.estado = 'Completado';
+        // Emitir actualización a todos los clientes
+        io.emit('estadoPlatoActualizado', { idPedido, idPlato });
+      }
+    }
+  });  
+
+  // Escuchar eventos de actualización de estado de pedido
+  socket.on('actualizarEstadoPedido', (data) => {
     const { idPedido, nuevoEstado } = data;
     const pedido = pedidos.find((p) => p.id === idPedido);
     if (pedido) {
       pedido.estado = nuevoEstado;
+      // Marcar todos los platos como completados
+      pedido.platos.forEach(plato => plato.estado = 'Completado');
       // Emitir actualización a todos los clientes
-      io.emit('estadoActualizado', pedido);
+      io.emit('estadoPedidoActualizado', pedido);
     }
   });
-});
+  
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
-
